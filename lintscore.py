b1dfb8b9284ddade8aa93447c459a779d86e6921
Christer Enfors <christer.enfors@gmail.com>
2016-11-18 13:38:44 +0100

    Now saves data properly, but no highscore list.

--- lintscore.py
+++ lintscore.py
@@ -17,22 +17,34 @@ class App(object):
     def run(self, file_name):
         "Start the application."
 
-        pylint_score = self.run_pylint(file_name)
-        num_lines = self.count_file_lines(file_name)
-        points = int(pylint_score * num_lines)
+        prev_score = self.database.get_file_score(file_name)
+        prev_num_lines = self.database.get_file_num_lines(file_name)
+        prev_points = self.calc_points(prev_score, prev_num_lines)
 
-        print("PyLint score: %f" % pylint_score)
-        print("Lines       : %d" % num_lines)
-        print("Final score : %d" % points)
+        score = self.run_pylint(file_name)
+        num_lines = self.count_file_lines(file_name)
+        points = self.calc_points(score, num_lines)
+        
+        print("prev_score      : %2.2f" % prev_score)
+        print("prev_num_lines  : %d" % prev_num_lines)
+        print("prev_points     : %d" % prev_points)
+        print("------------------------------")
+        print("score           : %2.2f" % score)
+        print("num_lines       : %d" % num_lines)
+        print("points          : %d" % points)
+        print("------------------------------")
+        print("score change    : %2.2f" % (score - prev_score))
+        print("num_lines change: %d" % (num_lines - prev_num_lines))
+        print("points change   : %d" % (points - prev_points))
 
         self.database.add_record("#000000", file_name, "Christer",
-                                 pylint_score, num_lines, points)
+                                 score, num_lines, points - prev_points)
 
     def run_pylint(self, file_name):
         "Run pylint on the specified file. Return the pylint score."
 
         results = Run([file_name], reporter=QuietReporter(), exit=False)
-        return results.linter.stats["global_note"]
+        return float(results.linter.stats["global_note"])
 
     def count_file_lines(self, file_name):
         "Return the number of lines the the specified file."
@@ -40,6 +52,11 @@ class App(object):
         with open(file_name, "r") as file_handle:
             return len(file_handle.readlines())
 
+
+    def calc_points(self, score, num_lines):
+        "Return points based on score and number of lines."
+        return int((score - 5.0) * num_lines)
+        
 
 class QuietReporter(BaseReporter):
     "Empty reporter to disable pylint output."
--- lintscore_db.py
+++ lintscore_db.py
@@ -23,55 +23,48 @@ class Database(object):
                         "COMMIT_ID     text,"
                         "FILE_NAME     text,"
                         "USER          text,"
-                        "PYLINT_SCORE  float,"
+                        "SCORE         float,"
                         "NUM_LINES     int,"
-                        "POINTS        int,"
                         "POINTS_CHANGE int,"
                         "TIME          timestamp);")
 
-    def add_record(self, commit_id, file_name, user, pylint_score, num_lines,
-                   points):
+
+    def add_record(self, commit_id, file_name, user, score, num_lines,
+                   points_change):
         "Add one record to the database."
         with self.con:
             cur = self.con.cursor()
 
-            print("-- POINTS: %d" % points)
-
-            prev_points = self.get_prev_points(file_name)
-            if prev_points:
-                points_change = points - prev_points
-                print("-- PREV_POINTS: %d" % prev_points)
-            else:
-                points_change = 0
-
-            print("-- POINTS_CHANGE: %d" % points_change)
-
             print("INSERT")
-            cur.execute("insert into RECORD "
-                        "(commit_id, file_name, user, pylint_score, num_lines, "
-                        "points, points_change, time) values "
-                        "(?, ?, ?, ?, ?, ?, ?, ?)",
-                        (commit_id, file_name, user, pylint_score,
-                         num_lines, points, points_change,
-                         datetime.datetime.now()))
+            cur.execute("insert into RECORD("
+                        "commit_id, file_name, user, score, num_lines, "
+                        "points_change, time) values (?, ?, ?, ?, ?, ?, ?)",
+                        (commit_id, file_name, user, score, num_lines,
+                         points_change, datetime.datetime.now()))
 
-    def get_prev_points(self, file_name):
+    def get_file_score(self, file_name):
         with self.con:
             cur = self.con.cursor()
 
-            cur.execute("select POINTS from RECORD where file_name = ? "
+            cur.execute("select SCORE from RECORD where file_name = ? "
                         "order by time desc limit 1", (file_name,))
 
-            return cur.fetchone()[0]
+            try:
+                return float(cur.fetchone()[0])
+            except TypeError:
+                return 0
 
-    def get_user_points_change(self, user):
+    def get_file_num_lines(self, file_name):
         with self.con:
             cur = self.con.cursor()
 
-            cur.execute("select sum(points_change) from RECORD where "
-                        "user = ?", (user,))
+            cur.execute("select NUM_LINES from RECORD where file_name = ? "
+                        "order by time desc limit 1", (file_name,))
 
-            return cur.fetchone()[0]
+            try:
+                return cur.fetchone()[0]
+            except TypeError:
+                return 0
 
     def get_all_users(self):
         pass
